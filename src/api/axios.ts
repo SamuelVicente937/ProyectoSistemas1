a359cbecd8fc9a10b7bc4c823f0ac357b6696546
samuel <vicentesamue937@gmail.com>
2025-12-01 08:32:35 -0400

    Fix Axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,14 +1,17 @@
 import axios, { AxiosError } from "axios";
 import type { InternalAxiosRequestConfig, AxiosInstance } from "axios";
 
+console.log("🔍 VITE_API_URL:", import.meta.env.VITE_API_URL);
+console.log("🔍 Todas las env:", import.meta.env);
 const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
-  withCredentials: true, 
+  withCredentials: true,
 });
+console.log('🔍 baseURL configurada:', api.defaults.baseURL);
 
 api.interceptors.request.use(
   (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
